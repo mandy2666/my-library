c6744f085784b5b67707f0c171a761933819d222
mandy2666 <146002294+mandy2666@users.noreply.github.com>
2024-04-15 14:11:16 +0530

    Update DockerUtils.groovy

--- vars/DockerUtils.groovy
+++ vars/DockerUtils.groovy
@@ -1,7 +1,10 @@
-// File: pipeline/vars/myStep.groovy
+// DockerUtils.groovy
 
-def loginToDockerHub(String username, String password) {
+def call(String username, String password) {
+    // Implement the logic to login to Docker Hub using the provided username and password
+    // For example:
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+        sh "docker login -u $username -p $password"
     }
 }
+
